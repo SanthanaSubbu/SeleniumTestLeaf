242cdf2fe2ee4380faef5c88c3fd930a7a61cb15
SanthanaSubbu <46918806+SanthanaSubbu@users.noreply.github.com>
2025-02-08 22:22:38 +0530

    Add files via upload

---
+++ week3/day4/ActionsAmazon.java
@@ -0,0 +1,63 @@
+package week3.day4;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsAmazon {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		ChromeDriver driver=new ChromeDriver();
+
+		driver.get("https://www.amazon.in/");
+
+		driver.manage().window().maximize();
+		driver.navigate().refresh();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("oneplus 9 pro");
+		 driver.findElement(By.id("nav-search-submit-button")).click();
+		 //List<WebElement> price = driver.findElements(By.xpath("//span[@class='a-price-whole']"));
+		 List<WebElement> price = driver.findElements(By.xpath("//span[@class='a-price']"));
+		 String cartValue= price.get(0).getText();
+		 System.out.println("Price of the first product: "+cartValue);
+		 String rating = driver.findElement(By.xpath("//a[@class='a-popover-trigger a-declarative'][1]")).getAttribute("aria-label");
+		 System.out.println("Rating for first product: "+rating);
+		 //Click on First product
+		 driver.findElement(By.xpath("//div[@data-cy='title-recipe'][1]")).click();
+		 Thread.sleep(2000);
+		 //Switch to the product window
+		 Set<String> allWindowAddress = driver.getWindowHandles();
+		 List<String> allWindows = new ArrayList<String>(allWindowAddress);
+		 driver.switchTo().window(allWindows.get(1));
+		 Thread.sleep(2000);
+		 //Take Screenshot of Product
+		 File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+         File destination = new File("C:/Users/santh/FinalChallenge/Training/screenshots/product.png");
+         FileUtils.copyFile(screenshot, destination);
+         //Add the product to cart
+        
+         WebElement cart = driver.findElement(By.id("add-to-cart-button"));
+         Actions a = new Actions(driver);
+         a.moveToElement(cart).click().perform();
+         Thread.sleep(2000);
+         String subTotal =driver.findElement(By.xpath("//span[@class='a-size-base-plus a-color-price a-text-bold']")).getText();
+         System.out.println(cartValue.toString());
+         System.out.println(subTotal.toString());
+         boolean isMatch = subTotal.contains(cartValue);
+         System.out.println("Product total verified: "+isMatch);
+         
+	}
+
+}
